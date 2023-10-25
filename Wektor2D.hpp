c67d343408e2e462fb659eab789bc7f4dacdf47e
Maron-dev <73064001+Maron-dev@users.noreply.github.com>
2023-10-25 17:26:27 +0200

    Add files via upload

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,12 +1,18 @@
 class Wektor2D
 {
-    private:
-        double x;
-        double y;
-    public:
-    Wektor2D() {x = 0; y = 0; }
+private:
+    double x;
+    double y;
+public:
+    Wektor2D()
+    {
+        //cout << "Stworzylem wektor doyslny";
+        x = 0;
+        y = 0;
+    }
     Wektor2D(double p_x, double p_y)
     {
+        //cout << "Stworzylem wektor niedomyslny";
         x = p_x;
         y = p_y;
     }
@@ -27,14 +33,3 @@ class Wektor2D
         return y;
     }
 };
-
-Wektor2D operator+(Wektor2D w1, Wektor2D w2)
-{
-    return Wektor2D(w1.getX() + w2.getX(), w2.getY()+w2.getY());
-}
-
-Wektor2D operator*(Wektor2D w1, Wektor2D w2)
-{
-    double result;
-    result = (w1.getX() * w2.getX()) + (w1.getY() * w2.getX());
-}
