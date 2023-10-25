9ac64de2ac1b1eed841ada30b5064c9db6270951
Maron-dev <73064001+Maron-dev@users.noreply.github.com>
2023-10-25 16:35:50 +0200

    Add files via upload

--- Wektor2D.hpp
+++ Wektor2D.hpp
@@ -1,4 +1,13 @@
 class Wektor2D
 {
-    // Tutaj napisz implementacje klasy Wektor2D
+    private:
+        double x;
+        double y;
+    public:
+    Wektor2D() {x = 0; y = 0; }
+    Wektor2D(double p_x, double p_y)
+    {
+        x = p_x;
+        y = p_y;
+    }
 };
